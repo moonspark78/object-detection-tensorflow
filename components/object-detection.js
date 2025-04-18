366d5dddcd9bba40d76146502f66702d4b5aeb3f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-19 00:57:50 +0200

    Working on  ObjectDetection components

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,9 +1,25 @@
 "use client"
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Webcam from 'react-webcam';
 
 const ObjectDetection = () => {
     const webcamRef = useRef(null);
+
+    const showmyVideo = () =>{
+        if(webcamRef.current !== null && webcamRef.current.video?.readyState === 4){
+            const myVideoWidth = webcamRef.current.video.videoWidth;
+            const myVideoHeight = webcamRef.current.video.videoHeight;
+
+            webcamRef.current.video.width = myVideoWidth;
+            webcamRef.current.video.height = myVideoHeight;
+
+        }
+    }
+
+    useEffect(() =>{
+        showmyVideo();
+    },[])
+
   return (
     <div className='mt-8'>
         <div className='relative flex justify-center items-center p-1.5 rounded-md'>
