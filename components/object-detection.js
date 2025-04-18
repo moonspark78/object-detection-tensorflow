f5fd58afc6d624e8c1b16235627e4f116cc6dd70
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-19 00:36:22 +0200

    Working on  ObjectDetection Components

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,10 +1,14 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
+import Webcam from 'react-webcam';
 
 const ObjectDetection = () => {
+    const webcamRef = useRef(null);
   return (
     <div className='mt-8'>
-        <div>
+        <div className='relative flex justify-center items-center p-1.5 rounded-md'>
             {/* Webcam */}
+            <Webcam ref={webcamRef} className='rounded-md w-full lg:h-[620px]' muted/>
             {/* Canvas */}
         </div>
     </div>
