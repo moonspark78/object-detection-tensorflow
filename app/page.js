f5963c6277b8f7e4b9ecf49a8701549ec7e91a2b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-04-18 23:34:17 +0200

    Working on page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-      <main className="">
+      <main className="flex min-h-screen flex-col items-center p-8">
         <h1>Detetion Alarm</h1>
       </main>
   );
